6aa270254e98ad1b60e8a56aadc55f2cc39646b5
justinrivera47 <justinrivera47@gmail.com>
2022-08-09 15:17:58 -0400

    done

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+const server = express();
+const postRouter = require('./posts/posts-router')
+
+server.use(express.json())
+server.use('/api/posts', postRouter)
+
+server.use('*', (req, res, next) => {
+  res.status(404).json({ 
+    message: "not found" 
+  })
+})
+
+module.exports = server
